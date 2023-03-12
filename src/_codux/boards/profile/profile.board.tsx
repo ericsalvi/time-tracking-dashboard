e5461ffabaf290e594baa7c6fbaec0d210153633
Eric Salvi <32136086+ericsalvi@users.noreply.github.com>
2023-03-12 11:39:16 -0400

    fine tuned the CSS for mobile for the profile card only

---
+++ src/_codux/boards/profile/profile.board.tsx
@@ -0,0 +1,7 @@
+import { createBoard } from '@wixc3/react-board';
+import { Profile } from '../../../components/profile/profile';
+
+export default createBoard({
+    name: 'Profile',
+    Board: () => <Profile />
+});
